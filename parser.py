ddf4650209d9db06120c0ca22c6464293ffe6df0
SnpstreD <agamalov.v@skillet.ru>
2024-05-20 19:02:54 +0300

    remastered class for character parser

--- parser.py
+++ parser.py
@@ -1,21 +1,40 @@
-# Яна жопка
+# import Яна_жопка
 import requests
 from bs4 import BeautifulSoup
 
-# Ссылка на персонажа:
-url = "https://animego.org/character/1530-shikamaru-nara"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
 
-# Словарь характеристик:
-character = {
-    'name': '',
-    'image': '',
-    'description': ''
-}
+class Character:
+    def __init__(self, url):
+        self.url = url
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+            'referer': 'https://www.google.com/'
+        }
+        self.response = requests.get(self.url, headers=self.headers)
+        self.soup = BeautifulSoup(self.response.content, "html.parser")
 
-# Заполнение характеристик
-for trait in character:
-    character[trait] = soup.find('img').get('src') if trait == 'image' else soup.find(itemprop=trait).getText().strip()
+    # Check for bad responses
+    def check_for_response(self):
+        if self.response.status_code == 200:
+            return True
+        else:
+            print(f'Bad response: {self.response}')
+            return False
 
-print(character)
+    # Fetch a link to the character's image
+    def image_link(self):
+        return f"https://anichars.ru{self.soup.find('img').get('src')}"
+
+    # Fetch all traits in a dictionary
+    def traits(self):
+        self.all_char_data = self.soup.find_all('div', class_='character__data')
+        return {x.find(class_='character__title').getText().strip(): x.find(class_='character__value').getText().strip()
+                for x in self.all_char_data}
+
+
+# Check:
+url = 'https://anichars.ru/characters/niko-robin'
+this_character = Character(url)
+if this_character.check_for_response():
+    print(this_character.traits())
+    print(this_character.image_link())
